657a09feddf87fb4161815a016a184023a7c23bf
oqsan-syan <osurenyan@bk.ru>
2021-01-14 00:59:49 +0400

    Updates

---
+++ precache-manifest.b5ca56c8514f252c8e8346fa12dc4c03.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0912e5d15750ce03e84a83c188dfb621",
+    "url": "/basketball/index.html"
+  },
+  {
+    "revision": "b256e92e05e814aeaa78",
+    "url": "/basketball/static/css/main.746fa296.chunk.css"
+  },
+  {
+    "revision": "5be33068402ba29a5938",
+    "url": "/basketball/static/js/2.5580fd91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basketball/static/js/2.5580fd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b256e92e05e814aeaa78",
+    "url": "/basketball/static/js/main.314f2092.chunk.js"
+  },
+  {
+    "revision": "07a134f2170ebc336533",
+    "url": "/basketball/static/js/runtime-main.ed9b9eb0.js"
+  },
+  {
+    "revision": "ec6d52fe8083e746b11785af85b0b7bc",
+    "url": "/basketball/static/media/bg.ec6d52fe.png"
+  },
+  {
+    "revision": "d71c1366d48922fbda675b5b1001c0c8",
+    "url": "/basketball/static/media/header-bg.d71c1366.png"
+  },
+  {
+    "revision": "66f3fca6fadc4c16300d34ef7a312112",
+    "url": "/basketball/static/media/left-c.66f3fca6.svg"
+  },
+  {
+    "revision": "ff81be57921ea27975bef8590b5993d5",
+    "url": "/basketball/static/media/right-c.ff81be57.svg"
+  }
+]);
